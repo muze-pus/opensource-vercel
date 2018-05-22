muze-pus/opensource-vercel

commit 54407403712987760a8a60b019ffb424996ac121

issue normal cert for nested subdomain (#1344)

src/providers/sh/commands/alias/create-cert-for-alias.js
@@ -76,7 +76,12 @@ function getCertRequestSettings(alias: string, domain: string, subdomain: string
       }
     }
   } else {
-    return { cns: [domain, `*.${domain}`], preferDNS: false }
+    if(subdomain.includes('.')) {
+      // Nested subdomains can't use wildcards
+      return { cns: [alias], preferDNS: true }
+    } else {
+      return { cns: [domain, `*.${domain}`], preferDNS: false }
+    }
   }
 }
 

commit 05caa43a44a512a75cf52ded49a93addf5abedb8

Make sure subdomain is correctly typed / checked (#1364)
Fixes an error introduced in #1344 when running `now alias` on a naked domain like `example.com`.

src/providers/sh/commands/alias/create-cert-for-alias.js
@@ -58,7 +58,7 @@ async function createCertificateForAlias(output: Output, now: Now, alias: string
   return cert
 }
 
-function getCertRequestSettings(alias: string, domain: string, subdomain: string, httpChallengeInfo?: HTTPChallengeInfo) {
+function getCertRequestSettings(alias: string, domain: string, subdomain: string | null, httpChallengeInfo?: HTTPChallengeInfo) {
   if (httpChallengeInfo) {
     if (subdomain === null) {
       if (httpChallengeInfo.canSolveForRootDomain) {
@@ -76,7 +76,7 @@ function getCertRequestSettings(alias: string, domain: string, subdomain: string
       }
     }
   } else {
-    if(subdomain.includes('.')) {
+    if(subdomain && subdomain.includes('.')) {
       // Nested subdomains can't use wildcards
       return { cns: [alias], preferDNS: false }
     } else {